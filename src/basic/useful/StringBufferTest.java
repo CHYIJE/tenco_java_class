77955f942771a217928c7d53ae7eaabf30a39b97
cnow <wjdtpdbs1887@naver.com>
2024-04-26 10:36:40 +0900

    String Buffer

---
+++ src/basic/useful/StringBufferTest.java
@@ -0,0 +1,32 @@
+package basic.useful;
+
+public class StringBufferTest {
+
+	// 코드의 시작점 (메인 작업자)
+	public static void main(String[] args) {
+		
+		String str1 = new String("Hello");
+		String str2 = new String("World");
+		
+		StringBuffer bufferStr = new StringBuffer("Hello");
+		System.out.println(bufferStr);
+		// 1.
+		System.out.println(System.identityHashCode(bufferStr)); // 원시 주소 값
+		
+		bufferStr.append(str2);
+		
+		System.out.println(bufferStr);
+		// 2.
+		System.out.println(System.identityHashCode(bufferStr));
+		
+		// 1번 결과와 2번 결과의 주소값이 같다라는 의미는
+		// 새로운 메모리를 할당한 것이 아닌 변경한 것이다.
+		
+		// 활용
+		String newStr = (String.valueOf(bufferStr));
+		System.out.println("-------------");
+		System.out.println(System.identityHashCode(newStr));
+		
+		
+	} // end of main
+} // end of class
