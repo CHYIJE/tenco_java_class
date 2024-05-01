15785dd26a2369c84197060fcdef9022eb92f79b
cnow <wjdtpdbs1887@naver.com>
2024-05-01 15:02:14 +0900

    thread 예제

---
+++ src/basic/useful/ch05/MyThread2.java
@@ -0,0 +1,28 @@
+package basic.useful.ch05;
+
+/**
+ *  쓰레드를 만드는 두번째 방법 Runable 인터페이스를
+ *  활용하는 방법
+ */
+public class MyThread2 implements Runnable {
+	
+	// Thread 클래스에서 start 메서드가 호출되면
+	// 동작하는 부분이라고 약속 되어있다.
+	@Override
+	public void run() {
+		int i = 0;
+		while (i < 10) {
+			System.out.println("i : " + i);
+			i++;
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	
+
+} // end of class
